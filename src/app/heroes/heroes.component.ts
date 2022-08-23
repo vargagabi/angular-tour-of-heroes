12117e4c67e3cfa31ae57a88bde0abebbee0f15d
gabor <vargagabi2209@gmail.com>
2022-08-23 11:15:28 +0200

    display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+	selector: 'app-heroes',
+	templateUrl: './heroes.component.html',
+	styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 0,
-    name: "Windstorm"
-  };
 
-  constructor() { }
+	selectedHero?: Hero;
+	HEROES: Hero[] = [
+		{id: 12, name: 'Dr. Nice'},
+		{id: 13, name: 'Bombasto'},
+		{id: 14, name: 'Celeritas'},
+		{id: 15, name: 'Magneta'},
+		{id: 16, name: 'RubberMan'},
+		{id: 17, name: 'Dynama'},
+		{id: 18, name: 'Dr. IQ'},
+		{id: 19, name: 'Magma'},
+		{id: 20, name: 'Tornado'}
+	];
 
-  ngOnInit(): void {
-  }
+	constructor() {
+	}
 
+	ngOnInit(): void {
+	}
+
+	onSelected(hero: Hero) {
+		this.selectedHero = hero;
+	}
 }
