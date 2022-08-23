13c7196f57528dd2ede6b0e28c18762b99e35e66
gabor <vargagabi2209@gmail.com>
2022-08-23 11:02:26 +0200

    the hero editor

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 0,
+    name: "Windstorm"
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
